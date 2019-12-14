a870a3b58b9f6b92a9bd238ac444f52fd9b4f467
stellie82 <kims82@gmail.com>
2019-12-14 12:00:01 -0800

    update with master branch

--- models/items.sql
+++ models/items.sql
@@ -3,8 +3,8 @@ USE cajarilla_db;
 INSERT INTO Items (itemName, itemCategory, itemDescription, itemState, itemPrice, itemPhoto, userName)
 VALUES
     ("Bookshelf", "Furniture", "test", "Used", 25.00, "https://target.scene7.com/is/image/Target/GUEST_0aeefc0e-2eef-47ae-bdd0-0ca60ed81100?wid=488&hei=488&fmt=pjpeg", "User 4"),
-    ("Chair", "Furniture", "test", "Gently Used", 15.00, "https://cdn.shopify.com/s/files/1/1920/4697/products/S27_15SM58-S_001.jpg?v=1562175112", "User 4"),
-    ("Blender", "Household", "test", "Gently Used", 10.00, "https://www.ikohs.com/uk/121542/batidora-ikohs-retro-line-bt-khea-15l-1000w.jpg", "User 4"),
-    ("Pillows", "Household", "test", "New", 20.00, "http://www.shopmarriott.com/images/products/v2/lrg/Marriott-feather-down-pillow-MAR-108_lrg.jpg", "User 4"),
+    ("Chair", "Furniture", "test", "Gently Used", 15.00, "https://cdn.shopify.com/s/files/1/1920/4697/products/S27_15SM58-S_001.jpg?v=1562175112", "User 1"),
+    ("Blender", "Household", "test", "Gently Used", 10.00, "https://www.ikohs.com/uk/121542/batidora-ikohs-retro-line-bt-khea-15l-1000w.jpg", "User 2"),
+    ("Pillows", "Household", "test", "New", 20.00, "http://www.shopmarriott.com/images/products/v2/lrg/Marriott-feather-down-pillow-MAR-108_lrg.jpg", "User 3"),
     ("Drill Set", "Tools", "test", "New", 50.00, "https://images-na.ssl-images-amazon.com/images/I/61T3ILWwUfL._AC_SX522_.jpg", "User 4");
     
