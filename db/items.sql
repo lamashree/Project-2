897f72bfcf0a4c0050b04b5019ec5dd53618cb87
stellie82 <kims82@gmail.com>
2019-12-16 17:39:43 -0800

    add more seeds data and update item design

--- db/items.sql
+++ db/items.sql
@@ -1,10 +1,21 @@
-USE cajarilla_db;
+USE cajarilla_db,
 
 INSERT INTO Items (itemName, itemCategory, itemDescription, itemState, itemPrice, itemPhoto, userName)
 VALUES
-    ("Bookshelf", "Furniture", "test", "Used", 25.00, "https://target.scene7.com/is/image/Target/GUEST_0aeefc0e-2eef-47ae-bdd0-0ca60ed81100?wid=488&hei=488&fmt=pjpeg", "User 4"),
-    ("Chair", "Furniture", "test", "Gently Used", 15.00, "https://cdn.shopify.com/s/files/1/1920/4697/products/S27_15SM58-S_001.jpg?v=1562175112", "User 1"),
-    ("Blender", "Household", "test", "Gently Used", 10.00, "https://www.ikohs.com/uk/121542/batidora-ikohs-retro-line-bt-khea-15l-1000w.jpg", "User 2"),
-    ("Pillows", "Household", "test", "New", 20.00, "http://www.shopmarriott.com/images/products/v2/lrg/Marriott-feather-down-pillow-MAR-108_lrg.jpg", "User 3"),
-    ("Drill Set", "Tools", "test", "New", 50.00, "https://images-na.ssl-images-amazon.com/images/I/61T3ILWwUfL._AC_SX522_.jpg", "User 4");
+    ("Bookshelf", "Furniture", "test", "Used", 25.00, "https://target.scene7.com/is/image/Target/GUEST_0aeefc0e-2eef-47ae-bdd0-0ca60ed81100?wid=488&hei=488&fmt=pjpeg", "User 1"),
+    ("Chair", "Furniture", "test", "Gently Used", 15.00, "https://cdn.shopify.com/s/files/1/1920/4697/products/S27_15SM58-S_001.jpg?v=1562175112", "User 2"),
+    ("Blender", "Household", "test", "Gently Used", 10.00, "https://www.ikohs.com/uk/121542/batidora-ikohs-retro-line-bt-khea-15l-1000w.jpg", "User 3"),
+    ("Pillows", "Household", "test", "New", 20.00, "http://www.shopmarriott.com/images/products/v2/lrg/Marriott-feather-down-pillow-MAR-108_lrg.jpg", "User 4"),
+    ("Drill Set", "Tools", "test","New", 50.00, "https://images-na.ssl-images-amazon.com/images/I/61T3ILWwUfL._AC_SX522_.jpg", "User 5"),
+    ("Dish Set", "Household", "Barely used dish set, very good condition", "Gently Used", 20.00, "https://images-na.ssl-images-amazon.com/images/I/817QArFCTPL._SX425_.jpg", "User 1"),
+    ("Nintendo Switch", "Video Games", "Switch console, fair condition.  Comes with case.", "Used", 100.00, "https://cdn.pocket-lint.com/r/s/320x/assets/images/140007-games-review-nintendo-switch-review-image1-lp6zy9awm0.jpg?v1","User 2"),
+    ("Acer Chromebook", "Electronics", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", "Used", 50.00, "https://target.scene7.com/is/image/Target/GUEST_edb5217e-cd3c-476e-810b-33b7eb576279?wid=488&hei=488&fmt=pjpeg", "User 3"),
+    ("Kettlebell", "Exercise", "test", "New", 20.00, "https://cdn11.bigcommerce.com/s-zvhwp/images/stencil/original/products/1022/2851/68kg-front__34853.1471233471.jpg?c=2&imbypass=on", "User 4"),
+    ("Baby Stroller", "Baby and Kids", "test", "New", 150.00, "https://images.ctfassets.net/50gzycvace50/bf4caa316fd0a6b2a98188fea06711e2e074085c9d2a40724df83e842284bb91/aa58d4251ae45077d51936b6497a9708/bf4caa316fd0a6b2a98188fea06711e2e074085c9d2a40724df83e842284bb91.png?fl=progressive&fm=jpg&bg=rgb:f9f9f9&w=620&h=620", "User 5"),
+    ("Baby Bottles", "Baby and Kids", "test", "New", 35.00, "https://target.scene7.com/is/image/Target/GUEST_25d70bdf-9892-4287-8741-639541b9cbbd?wid=488&hei=488&fmt=pjpeg", "User 1"),
+    ("Original Monopoly", "Games and Toys", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vestibulum sed arcu non odio. Nibh cras pulvinar mattis nunc sed blandit libero volutpat. Faucibus et molestie ac feugiat sed lectus vestibulum mattis. Arcu risus quis varius quam quisque. Vitae aliquet nec ullamcorper sit amet risus nullam. Nulla pellentesque dignissim enim sit amet venenatis urna cursus eget. Id nibh tortor id aliquet. Libero justo laoreet sit amet cursus sit. Quis hendrerit dolor magna eget est lorem. Id diam maecenas ultricies mi eget mauris.", "Used", 5.00, "https://i.ebayimg.com/images/g/mOIAAOSwtM9cait2/s-l300.jpg", "User 2"),
+    ("Basic Lego Set", "Games and Toys", "test", "Gently Used", 20.00, "https://eugenetoyandhobby.com/wp-content/uploads/2019/02/11002-1.jpg", "User 3"),
+    ("Guitar", "Music", "test", "Used", 100.00, "https://i.ebayimg.com/images/g/~OAAAOSwOPldVtZP/s-l640.jpg", "User 4"),
+    ("Ear Buds", "Music", "test", "Used", 15.00, "https://o.aolcdn.com/images/dims?quality=85&image_uri=http%3A%2F%2Fo.aolcdn.com%2Fhss%2Fstorage%2Fmidas%2Faae24bfb1116c13ee9540adf9e663d41%2F202963190%2Frevolsjt.jpg&client=amp-blogside-v2&signature=eb249ef2d824acd813309d24a03c12ce7994eb5d", "User 5");
     
+
