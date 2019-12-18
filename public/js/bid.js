2a698a9f7b054660ca470d55ff20ded81c2fcd6b
jsills3110 <jsills3110@gmail.com>
2019-12-17 19:26:37 -0800

    Bids now populate below an item when a new bid is submitted.

--- public/js/bid.js
+++ public/js/bid.js
@@ -1,32 +1,72 @@
+// Get references to page elements
+var $bidList = $("#bid-list");
+
+var getBids = function(itemId) {
+  return $.ajax({
+    url: "/api/items/" + itemId + "/bids",
+    type: "GET"
+  });
+};
+
 var putBid = function(bid) {
-    return $.ajax({
-      headers: {
-        "Content-Type": "application/json"
-      },
-      type: "POST",
-      url: "/api/bids",
-      data: JSON.stringify(bid)
+  return $.ajax({
+    headers: {
+      "Content-Type": "application/json"
+    },
+    type: "POST",
+    url: "/api/bids",
+    data: JSON.stringify(bid)
+  });
+};
+
+// handleBidSubmit is called whenever we submit a new bid to the database.
+// Save the new bid to the db and refresh the list
+var handleBidSubmit = function(event) {
+  event.preventDefault();
+  console.log("form is working");
+
+  var item = {
+    bidValue: $("#newBid")
+      .val()
+      .trim(),
+    userName: $("#bid_username")
+      .val()
+      .trim(),
+    ItemId: $("#item_id")[0].innerHTML
+  };
+
+  putBid(item).then(function() {
+    refreshBids();
+  });
+  // location.reload();
+};
+
+function refreshBids() {
+  var itemId = $("#item_id")[0].innerHTML;
+  getBids(itemId).then(function(data) {
+    var $bids = data.map(function(bids) {
+      var $userName = $("<p>").text(bids.userName);
+
+      var $bidValue = $("<p>").text(bids.bidValue);
+
+      var $li = $("<li>")
+        .attr({
+          class: "list-group-item",
+          "data-id": bids.id
+        })
+        .append($userName);
+      $li.append($bidValue);
+
+      return $li;
     });
-  };
-  
-  // handleBidSubmit is called whenever we submit a new bid to the database.
-  // Save the new bid to the db and refresh the list
-  var handleBidSubmit = function(event) {
-    event.preventDefault();
-    console.log("form is working");
-  
-    var item = {
-      bidValue: $("#newBid")
-        .val()
-        .trim(),
-      userName: $("#bid_username")
-        .val()
-        .trim(),
-      ItemId: $("#item_id")[0].innerHTML
-    };
-    putBid(item);
-    location.reload();
-  };
-  
+    $bidList.empty();
+    $bidList.append($bids);
+  });
+}
+
+$(document).ready(function() {
+  refreshBids();
+
   // Add event listeners to the submit and delete buttons
   $("#submitBid").on("click", handleBidSubmit);
+});
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,31 +1,38 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
+  // Get all items
   app.get("/api/items", function(req, res) {
     db.Items.findAll({}).then(function(dbItems) {
       res.json(dbItems);
     });
   });
 
-  // Create a new example
+  // Create a new item
   app.post("/api/items", function(req, res) {
     db.Items.create(req.body).then(function(dbItem) {
       res.json(dbItem);
     });
   });
 
-  // Delete an example by id
+  // Delete an item by id
   app.delete("/api/items/:id", function(req, res) {
     db.Items.destroy({ where: { id: req.params.id } }).then(function(dbItem) {
       res.json(dbItem);
     });
   });
-  
+
   // Create a new bid
   app.post("/api/bids", function(req, res) {
     db.Bids.create(req.body).then(function(dbItem) {
       res.json(dbItem);
+    });
+  });
+
+  // Get all bids for an item
+  app.get("/api/items/:id/bids", function(req, res) {
+    db.Bids.findAll({ where: { ItemId: req.params.id } }).then(function(dbBids) {
+      res.json(dbBids);
     });
   });
 };
